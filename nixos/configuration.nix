320efc0cd5a4fe7bb29a7e0d5f1b24dcce4f3935
tupakkatapa <jeskar95@outlook.com>
2023-01-17 13:53:38 +0000

    add ssh public key

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -71,6 +71,7 @@
       isNormalUser = true;
       openssh.authorizedKeys.keys = [
         # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKEdpdbTOz0h9tVvkn13k1e8X7MnctH3zHRFmYWTbz9T kari@torque"
       ];
       # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
       extraGroups = [ "wheel" "networkmanager" "video" "audio" ];
