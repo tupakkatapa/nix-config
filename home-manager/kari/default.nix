5815ba0d5e18923f75f0d388c11e7d8313bc6ffe
tupakkatapa <jesse@ponkila.com>
2023-08-01 12:14:46 +0300

    kari: organize hm cfg

--- home-manager/kari/default.nix
+++ home-manager/kari/default.nix
@@ -11,12 +11,12 @@ with lib; {
     isNormalUser = true;
     group = "kari";
     extraGroups = [
-      "wheel"
-      "users"
-      "video"
       "audio"
       "input"
       "jackaudio"
+      "users"
+      "video"
+      "wheel"
     ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKEdpdbTOz0h9tVvkn13k1e8X7MnctH3zHRFmYWTbz9T kari@torque"
@@ -26,70 +26,36 @@ with lib; {
   users.groups.kari = {};
   environment.shells = [pkgs.fish];
 
-  # Creating some directories, these are persistent
-  systemd.tmpfiles.rules = [
-    "d /home/kari/.ssh 755 kari kari -"
-    "d /home/kari/Workspace 755 kari kari -"
-    "d /home/kari/Pictures/Screenshots 755 kari kari -"
-  ];
+  security.pam.services = {swaylock = {};};
+  programs.fish.enable = true;
 
   fonts.packages = with pkgs; [
     jetbrains-mono
     font-awesome
   ];
 
-  # services.user.mounts = [
-  #   {
-  #     enable = true;
-  #     description = "Backup disk";
-
-  #     what = "/dev/disk/by-id/60c4a67d-166b-48d2-a1c2-e457850a87df";
-  #     where = "/mnt/3TB";
-  #     type = "ext4";
-  #     options = "defaults";
-  #     wantedBy = [ "multi-user.target" ];
-  #   }
-  #   {
-  #     enable = true;
-  #     description = "SFTP mount";
-
-  #     what = "sftp_user@vladof:/root";
-  #     where = "/mnt/SFTP";
-  #     type = "fuse.sshfs";
-  #     options = "identityfile=/home/kari/.ssh/id_ed25519,allow_other,ssh_command=kari@vladof";
-  #     wantedBy = [ "multi-user.target" ];
-  #   }
-  # ];
-
   # Hyprland
   home-manager.sharedModules = [
     inputs.hyprland.homeManagerModules.default
   ];
-  programs = {
-    hyprland = {
-      enable = true;
-      package = inputs.hyprland.packages.${pkgs.system}.hyprland;
-    };
-  };
-  environment.sessionVariables = {
-    MOZ_ENABLE_WAYLAND = "1";
-    QT_QPA_PLATFORM = "wayland";
-    LIBSEAT_BACKEND = "logind";
-    WLR_NO_HARDWARE_CURSORS = "1";
-    WLR_RENDERER_ALLOW_SOFTWARE = "1";
+  programs.hyprland = {
+    enable = true;
+    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
   };
 
-  security.pam.services = {swaylock = {};};
-
-  # Fish
-  programs.fish.enable = true;
+  # Creating some directories, these are persistent
+  systemd.tmpfiles.rules = [
+    "d /home/kari/.ssh 755 kari kari -"
+    "d /home/kari/Pictures/Screenshots 755 kari kari -"
+    "d /home/kari/Workspace 755 kari kari -"
+  ];
 
   # Home-manager
   home-manager.users.kari = {
     imports = [
       # GUI Apps
-      ./config/librewolf.nix
       ./config/alacritty.nix
+      ./config/librewolf.nix
       ./config/vscode.nix
 
       # CLI Apps
@@ -97,40 +63,39 @@ with lib; {
       ./config/neovim.nix
 
       # WM Apps
-      ./config/hyprland
-      ./config/waybar.nix
       ./config/dunst.nix
-      ./config/swaylock.nix
-      ./config/swayidle.nix
-      ./config/wofi.nix
       ./config/gtk.nix
+      ./config/hyprland
+      ./config/swayidle.nix
+      ./config/swaylock.nix
+      ./config/waybar.nix
+      ./config/wofi.nix
     ];
 
     # Qjackctl presets
     home.file = {
-      "focusrite-guitarix.xml".source = ./config/qjackctl/focusrite-guitarix.xml;
-      "focusrite-guitarix-ardour-2tracks.xml".source = ./config/qjackctl/focusrite-guitarix-ardour-2tracks.xml;
+      "focusrite_guitarix_v2.xml".source = ./config/qjackctl/focusrite_guitarix_v2.xml;
     };
 
     home.packages = with pkgs; [
       # GUI Apps
       discord
       ferdium
+      nsxiv
+      openrgb
       plex-media-player
       plexamp
+      qemu
+      rpi-imager
       solaar
       ventoy
-      rpi-imager
-      openrgb
       xfce.thunar
       xfce.thunar-archive-plugin
-      nsxiv
-      qemu
 
       # Music stuff
+      ardour
       guitarix
       qjackctl
-      ardour
 
       # High quality games
       osu-lazer
@@ -140,21 +105,21 @@ with lib; {
       exa
       gnupg
       htop
+      iputils
       kexec-tools
       lshw
+      neofetch
       nix
       nix-tree
       ripgrep
       rsync
       ssh-to-age
+      sshfs
+      tmux
       vim
       wget
       wireguard-tools
       yt-dlp
-      sshfs
-      tmux
-      iputils
-      neofetch
 
       # Wofi / Waybar / Hyrpland deps
       blueberry
