01178b86e67671d1805c5a20c6c7c65897286da7
tupakkatapa <jesse@ponkila.com>
2023-08-24 21:12:10 +0300

    rename pkgs dir

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,6 +1,6 @@
 {inputs, ...}: {
   # This one brings our custom packages from the 'pkgs' directory
-  additions = final: _prev: import ../pkgs {pkgs = final;};
+  additions = final: _prev: import ../packages {pkgs = final;};
 
   # This one contains whatever you want to overlay
   modifications = final: prev: {
