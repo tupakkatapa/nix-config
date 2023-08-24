a3d649db6cc7d619cbebf0067be7f866c39f4bea
tupakkatapa <jesse@ponkila.com>
2023-08-24 18:12:42 +0300

    kari, vscode: rainbow-brackets do not work with nix files

--- home-manager/users/kari/config/vscode.nix
+++ home-manager/users/kari/config/vscode.nix
@@ -3,21 +3,22 @@
   programs.vscode = {
     enable = true;
     package = pkgs.vscodium;
-    extensions = with pkgs.vscode-extensions;
-      [
+    extensions =
+      with pkgs.vscode-extensions; [
         bbenoist.nix
         timonwong.shellcheck
         dart-code.flutter
         rust-lang.rust-analyzer
       ]
-      ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-        {
-          name = "rainbow-brackets-2";
-          publisher = "tejasvi";
-          version = "1.1.0";
-          sha256 = "sha256-07ZIZD8Bt1Z1hhs+AM2LYSEFMNRqjZFog6H0bGqblLs=";
-        }
-      ];
+      # ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      #   {
+      #     name = "rainbow-brackets-2";
+      #     publisher = "tejasvi";
+      #     version = "1.1.0";
+      #     sha256 = "sha256-07ZIZD8Bt1Z1hhs+AM2LYSEFMNRqjZFog6H0bGqblLs=";
+      #   }
+      # ]
+      ;
     enableUpdateCheck = false;
     enableExtensionUpdateCheck = false;
     userSettings = {
