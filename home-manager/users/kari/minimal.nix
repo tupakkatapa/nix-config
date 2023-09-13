48e56d2d6d1d1fef36311c3485f79443a8ade926
tupakkatapa <jesse@ponkila.com>
2023-09-14 02:08:09 +0300

    kari, minimal: init

---
+++ home-manager/users/kari/minimal.nix
@@ -0,0 +1,28 @@
+{
+  pkgs,
+  config,
+  inputs,
+  lib,
+  ...
+}: let
+  user = "kari";
+in {
+  # User config, setting password manually
+  users.users.${user} = {
+    isNormalUser = true;
+    group = "${user}";
+    extraGroups = [
+      "audio"
+      "video"
+      "wheel"
+      "users"
+    ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKEdpdbTOz0h9tVvkn13k1e8X7MnctH3zHRFmYWTbz9T kari@torque"
+    ];
+    shell = pkgs.fish;
+  };
+  users.groups.${user} = {};
+  environment.shells = [pkgs.fish];
+  programs.fish.enable = true;
+}
