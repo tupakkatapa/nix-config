81d4c3a65fe0bba2209718ef47874022b6bc1c88
tupakkatapa <jesse@ponkila.com>
2023-11-30 06:42:32 +0200

    rm buidl0

--- flake.nix
+++ flake.nix
@@ -7,12 +7,12 @@
     extra-substituters = [
       "https://cache.nixos.org"
       "https://nix-community.cachix.org"
-      "http://buidl0.ponkila.com:5000"
+      "http://torque.coditon.com:5000"
     ];
     extra-trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "buidl0.ponkila.com:qJZUo9Aji8cTc0v6hIGqbWT8sy+IT/rmSKUFTfhVGGw="
+      "torque.coditon.com:deBXOnPXp2vEHu4BAvh7TY2aUIOhT481ohsECftxO0E="
     ];
   };
 
--- system/nix-settings.nix
+++ system/nix-settings.nix
@@ -23,14 +23,14 @@
       extra-substituters = [
         "https://cache.nixos.org"
         "https://nix-community.cachix.org"
-        "http://buidl0.ponkila.com:5000"
         "https://ezkea.cachix.org"
+        "http://torque.coditon.com:5000"
       ];
       extra-trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "buidl0.ponkila.com:qJZUo9Aji8cTc0v6hIGqbWT8sy+IT/rmSKUFTfhVGGw="
         "ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI="
+        "torque.coditon.com:deBXOnPXp2vEHu4BAvh7TY2aUIOhT481ohsECftxO0E="
       ];
 
       # Allows this server to be used as a remote builder
@@ -41,19 +41,19 @@
       ];
     };
 
-    buildMachines = [
-      {
-        # For this to work, the host should be listed in the known hosts for the root user
-        # To do this, run: sudo su -c 'ssh kari@buidl0.ponkila.com -i /root/.ssh/id_ed25519'
-        systems = ["aarch64-linux" "i686-linux" "x86_64-linux"];
-        supportedFeatures = ["benchmark" "big-parallel" "kvm" "nixos-test"];
-        sshUser = "kari";
-        protocol = "ssh";
-        sshKey = "/root/.ssh/id_ed25519";
-        hostName = "buidl0.ponkila.com";
-        maxJobs = 20;
-      }
-    ];
+    # buildMachines = [
+    #   {
+    #     # For this to work, the host should be listed in the known hosts for the root user
+    #     # To do this, run: sudo su -c 'ssh kari@buidl0.ponkila.com -i /root/.ssh/id_ed25519'
+    #     systems = ["aarch64-linux" "i686-linux" "x86_64-linux"];
+    #     supportedFeatures = ["benchmark" "big-parallel" "kvm" "nixos-test"];
+    #     sshUser = "kari";
+    #     protocol = "ssh";
+    #     sshKey = "/root/.ssh/id_ed25519";
+    #     hostName = "buidl0.ponkila.com";
+    #     maxJobs = 20;
+    #   }
+    # ];
     distributedBuilds = true;
     # optional, useful when the builder has a faster internet connection than yours
     extraOptions = ''
