63c531f171d5b2e3c8f1e823ac85b7ac885120c2
tupakkatapa <jesse@ponkila.com>
2023-12-05 18:38:58 +0200

    revisit firefox

---
+++ home-manager/users/kari/config/firefox/bookmarks.nix
@@ -0,0 +1,201 @@
+[
+  {
+    name = "toolbar";
+    toolbar = true;
+    bookmarks = [
+      {
+        name = "home";
+        bookmarks = [
+          {
+            name = "Plex";
+            url = "https://plex.coditon.com";
+          }
+          {
+            name = "Radarr";
+            url = "https://radarr.coditon.com";
+          }
+          {
+            name = "Share";
+            url = "https://share.coditon.com";
+          }
+          {
+            name = "Torrent";
+            url = "https://torrent.coditon.com";
+          }
+        ];
+      }
+      {
+        name = "torrent";
+        bookmarks = [
+          {
+            name = "TorrentDay";
+            url = "https://www.torrentday.com/t";
+          }
+          {
+            name = "Nyaa";
+            url = "https://nyaa.si/";
+          }
+          {
+            name = "rutracker";
+            url = "https://rutracker.org/forum/index.php";
+          }
+        ];
+      }
+      {
+        name = "tools";
+        bookmarks = [
+          {
+            name = "Assets";
+            bookmarks = [
+              {
+                name = "Font Awesome";
+                url = "https://fontawesome.com/icons";
+              }
+              {
+                name = "Icons";
+                url = "https://techicons.dev/";
+              }
+              {
+                name = "Excalidraw";
+                url = "https://excalidraw.com/";
+              }
+              {
+                name = "FIGlet";
+                url = "https://patorjk.com/software/taag";
+              }
+            ];
+          }
+          {
+            name = "Music";
+            bookmarks = [
+              {
+                name = "metronomus";
+                url = "https://metronom.us/en/";
+              }
+              {
+                name = "guitarscale";
+                url = "https://guitarscale.org/bass/index.html";
+              }
+              {
+                name = "tap tempo";
+                url = "https://taptempo.io/";
+              }
+              {
+                name = "musmath";
+                url = "https://musmath.com/";
+              }
+            ];
+          }
+          {
+            name = "Dochub";
+            url = "https://dochub.com/";
+          }
+          {
+            name = "DeepL";
+            url = "https://www.deepl.com/translator";
+          }
+        ];
+      }
+      {
+        name = "docs";
+        bookmarks = [
+          {
+            name = "Kernel Config Search";
+            url = "https://www.kernelconfig.io/index.html";
+          }
+          {
+            name = "Vim Cheat Sheet";
+            url = "https://vim.rtorr.com/";
+          }
+          {
+            name = "Kea DHCP";
+            url = "https://kea.readthedocs.io/en/kea-2.4.1/";
+          }
+          {
+            name = "Hyprland";
+            url = "https://wiki.hyprland.org/";
+          }
+          {
+            name = "iPXE";
+            url = "https://ipxe.org/docs";
+          }
+        ];
+      }
+      {
+        name = "nix";
+        bookmarks = [
+          {
+            name = "Projects";
+            bookmarks = [
+              {
+                name = "Misterio77/nix-config";
+                url = "https://github.com/Misterio77/nix-config";
+              }
+              {
+                name = "jhvst/nix-config";
+                url = "https://github.com/jhvst/nix-config";
+              }
+            ];
+          }
+          {
+            name = "Nix Reference Manual";
+            bookmarks = [
+              {
+                name = "Built-in Functions";
+                url = "https://nixos.org/manual/nix/unstable/language/builtins.html";
+              }
+              {
+                name = "Data Types";
+                url = "https://nixos.org/manual/nix/unstable/language/values.html";
+              }
+            ];
+          }
+          {
+            name = "NixOS Search";
+            url = "https://search.nixos.org/packages?";
+          }
+          {
+            name = "Home Manager Search";
+            url = "https://mipmip.github.io/home-manager-option-search/";
+          }
+          {
+            name = "NixOS Wiki";
+            url = "https://nixos.wiki/";
+          }
+          {
+            name = "Flake Parts";
+            url = "https://flake.parts/";
+          }
+          {
+            name = "Nixvim docs";
+            url = "https://nix-community.github.io/nixvim/";
+          }
+        ];
+      }
+      {
+        name = "..";
+        bookmarks = [];
+      }
+      {
+        name = "YouTube";
+        url = "https://www.youtube.com/feed/subscriptions";
+      }
+      {
+        name = "Twitch";
+        url = "https://www.twitch.tv/directory";
+      }
+      {
+        name = "GitHub";
+        url = "https://github.com/";
+      }
+      {
+        name = "ChatGPT";
+        url = "https://chat.openai.com/";
+      }
+      {
+        name = "Hacker News";
+        url = "https://news.ycombinator.com/news";
+      }
+    ];
+  }
+]
