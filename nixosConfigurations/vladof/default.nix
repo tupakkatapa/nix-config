4caebbde9ec8ab53e74c9257e0b49963e7b1fb0e
tupakkatapa <jesse@ponkila.com>
2023-12-08 07:37:51 +0200

    vladof: wg mullvad

--- nixosConfigurations/vladof/default.nix
+++ nixosConfigurations/vladof/default.nix
@@ -143,6 +143,58 @@ in {
     ];
   };
 
+  # VPN
+  # https://alberand.com/nixos-wireguard-vpn.html
+  networking.wg-quick.interfaces = {
+    wg0 = {
+      address = ["10.66.219.228/32"];
+      listenPort = 51820;
+      privateKeyFile = "/mnt/wd-red/secrets/mullvad-vpn.key";
+      dns = [
+        "192.168.1.1"
+        "10.64.0.1"
+      ];
+      peers = [
+        {
+          publicKey = "C3jAgPirUZG6sNYe4VuAgDEYunENUyG34X42y+SBngQ=";
+          allowedIPs = ["0.0.0.0/0"];
+          endpoint = "193.32.127.69:51820";
+          persistentKeepalive = 25;
+        }
+      ];
+      postUp = ''
+        # Mark packets on the wg0 interface
+        wg set wg0 fwmark 51820
+
+        # Forbid anything else which doesn't go through wireguard VPN on
+        # ipV4 and ipV6
+        ${pkgs.iptables}/bin/iptables -A OUTPUT \
+          ! -d 192.168.0.0/16 \
+          ! -o wg0 \
+          -m mark ! --mark $(wg show wg0 fwmark) \
+          -m addrtype ! --dst-type LOCAL \
+          -j REJECT
+        ${pkgs.iptables}/bin/ip6tables -A OUTPUT \
+          ! -o wg0 \
+          -m mark ! --mark $(wg show wg0 fwmark) \
+          -m addrtype ! --dst-type LOCAL \
+          -j REJECT
+      '';
+      postDown = ''
+        ${pkgs.iptables}/bin/iptables -D OUTPUT \
+          ! -o wg0 \
+          -m mark ! --mark $(wg show wg0 fwmark) \
+          -m addrtype ! --dst-type LOCAL \
+          -j REJECT
+        ${pkgs.iptables}/bin/ip6tables -D OUTPUT \
+          ! -o wg0 -m mark \
+          ! --mark $(wg show wg0 fwmark) \
+          -m addrtype ! --dst-type LOCAL \
+          -j REJECT
+      '';
+    };
+  };
+
   # Message of the day
   programs.rust-motd = {
     enable = true;
