d22655b528e911323ef389dd5df8ac2f713a95ae
tupakkatapa <jesse@ponkila.com>
2024-02-10 14:47:17 +0200

    flake: comment out local substituter

--- flake.nix
+++ flake.nix
@@ -7,12 +7,12 @@
     extra-substituters = [
       "https://cache.nixos.org"
       "https://nix-community.cachix.org"
-      "http://torque.coditon.com:5000"
+      #"http://torque.coditon.com:5000"
     ];
     extra-trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "torque.coditon.com:deBXOnPXp2vEHu4BAvh7TY2aUIOhT481ohsECftxO0E="
+      #"torque.coditon.com:deBXOnPXp2vEHu4BAvh7TY2aUIOhT481ohsECftxO0E="
     ];
     extraOptions = ''
       download-attempts = 3
