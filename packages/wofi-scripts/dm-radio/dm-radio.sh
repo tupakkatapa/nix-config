13892d604d0f5fd476d8c478a5d54ebc9d503d57
tupakkatapa <jesse@ponkila.com>
2024-02-29 22:37:29 +0200

    dm-radio: improve, audio only

--- packages/wofi-scripts/dm-radio/dm-radio.sh
+++ packages/wofi-scripts/dm-radio/dm-radio.sh
@@ -3,13 +3,56 @@
 
 DMENU="wofi --dmenu -w 1"
 DMRADIOVOLUME="30"
+VIDEO=false
 
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <path_to_stations_json>"
+# Display usage information
+display_usage() {
+  cat <<USAGE
+Usage: dm-radio [OPTIONS] <path_to_stations_json>
+
+Description:
+  Opens an interactive menu to select and play radio stations from a specified JSON file.
+
+Options:
+  -v, --video
+    Enable video output for stations that support it.
+
+USAGE
+}
+
+# Parse and validate command line arguments
+parse_arguments() {
+  while [[ $# -gt 0 ]]; do
+    case "$1" in
+    -v|--video)
+      VIDEO=true
+      shift
+      ;;
+    -h|--help)
+      display_usage
+      exit 0
+      ;;
+    *)
+      if [[ -n $1 ]] && [[ ! $1 =~ ^- ]]; then
+        RADIO_STATIONS_FILE="$1"
+        shift
+      else
+        echo "Unknown option: $1"
+        display_usage
+        exit 1
+      fi
+      ;;
+    esac
+  done
+
+  if [[ -z $RADIO_STATIONS_FILE ]]; then
+    echo "Error: You must specify the path to the stations JSON file."
+    display_usage
     exit 1
-fi
+  fi
+}
 
-RADIO_STATIONS_FILE="$1"
+parse_arguments "$@"
 
 menu() {
     printf '%s\n' "quit"
@@ -30,11 +73,13 @@ main() {
         pkill -f http || echo "mpv not running."
         notify-send "Starting dm-radio" "Playing station: $choice. 🎶"
         URL=$(jq -r --arg choice "$choice" '.[] | select(.name==$choice) | .url' < "${RADIO_STATIONS_FILE}")
-        mpv --vf=format=yuv420p,scale=1280:720 --autofit=20% --volume="${DMRADIOVOLUME:-100}" "$URL"
+        if [ "$VIDEO" = true ]; then
+          mpv --vf=format=yuv420p,scale=1280:720 --autofit=20% --volume="${DMRADIOVOLUME:-100}" "$URL"
+        else
+          mpv --no-video --volume="${DMRADIOVOLUME:-100}" "$URL"
+        fi
         ;;
     esac
 }
 
-main "$@"
-
-
+main
