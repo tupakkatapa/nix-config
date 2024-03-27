0107d478c73beb52f55f008eef458f8b8fd8f768
tupakkatapa <jesse@ponkila.com>
2024-03-27 03:09:21 +0200

    vladof: fix index CSS

--- nixosConfigurations/vladof/services/index.nix
+++ nixosConfigurations/vladof/services/index.nix
@@ -10,58 +10,62 @@
     <head>
       <title>Service Index</title>
       <style>
-        body, html {
-          font-family: 'IBM Plex Mono', sans-serif;
-          background-color: #121212;
-          color: #fff;
-          margin: 0;
-          padding: 0;
-          display: flex;
-          height: 100vh;
-          align-items: flex-start;
-          justify-content: center;
-        }
-        #container {
-          max-width: 900px;
-          min-width: 400px;
-          background-color: #1f1f1f;
-          margin: 0 auto;
-          font-size: 16px;
-          line-height: 1.6;
-          color: #e8e8e8;
-          padding: 30px 75px;
-          border-radius: 10px;
-          border: 1px solid #333;
-          transform: translate(0, 33vh);
-        }
-        h1 {
-          color: #eee;
-          font-size: 24px;
-          margin-top: 24px;
-          margin-bottom: 16px;
-          font-weight: 700;
-          line-height: 1.25;
-        }
-        ul {
-          list-style-type: none;
-          padding: 0;
-        }
-        li {
-          padding: 10px 0;
-          border-bottom: 1px solid #2A2A2A;
-        }
-        li:last-child {
-          border-bottom: none;
-        }
-        a {
-          text-decoration: none;
-          color: #1e90ff;
-          transition: color 0.3s ease;
-        }
-        a:hover {
-          text-decoration: underline;
-          color: #63B3ED;
-        }
+          body, html {
+              font-family: 'IBM Plex Mono', sans-serif;
+              background-color: #121212;
+              color: #fff;
+              margin: 0;
+              padding: 0;
+              height: 100vh;
+              overflow: auto;
+          }
+          #container {
+              position: absolute;
+              top: 50px;
+              left: 0;
+              right: 0;
+              width: 900px;
+              margin: auto;
+              background-color: #1f1f1f;
+              font-size: 16px;
+              line-height: 1.6;
+              color: #e8e8e8;
+              padding-top: 30px;
+              padding-bottom: 40px;
+              padding-left: 75px;
+              padding-right: 75px;
+              border-radius: 10px;
+              border: 1px solid #333;
+              overflow-y: auto;
+          }
+          h1 {
+              color: #eee;
+              font-size: 24px;
+              margin-top: 24px;
+              margin-bottom: 16px;
+              font-weight: 700;
+              line-height: 1.25;
+          }
+          ul {
+              list-style-type: none;
+              padding: 0;
+          }
+          li {
+              padding: 10px 0;
+              border-bottom: 1px solid #2A2A2A;
+          }
+          li:last-child {
+              border-bottom: none;
+          }
+          a {
+              text-decoration: none;
+              color: #1e90ff;
+              transition: color 0.3s ease;
+          }
+          a:hover {
+              text-decoration: underline;
+              color: #63B3ED;
+          }
       </style>
     </head>
     <body>
