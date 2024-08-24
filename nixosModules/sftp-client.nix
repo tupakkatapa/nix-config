6f24c1e5c4101e1d26e10eed92fc3e8e2edff19d
tupakkatapa <jesse@ponkila.com>
2024-08-24 18:48:30 +0300

    nixosModules, sftpClient: add binds submodule

--- nixosModules/sftp-client.nix
+++ nixosModules/sftp-client.nix
@@ -1,19 +1,17 @@
-{ lib
-, pkgs
-, config
-, ...
-}:
+{ lib, pkgs, config, ... }:
+
 let
   cfg = config.services.sftpClient;
 in
 {
   options.services.sftpClient = {
-    enable = lib.mkEnableOption "SFTP Client";
+    enable = lib.mkEnableOption "Enable the SFTP client service.";
 
     defaultIdentityFile = lib.mkOption {
       type = lib.types.str;
       default = "";
-      description = "Default SSH identity file for the SFTP client.";
+      description = "Path to the default SSH identity file used by the SFTP client.";
+      example = "/home/user/.ssh/id_rsa";
     };
 
     mounts = lib.mkOption {
@@ -22,25 +20,53 @@ in
           identityFile = lib.mkOption {
             type = lib.types.str;
             default = cfg.defaultIdentityFile;
-            description = "SSH identity file for the SFTP client.";
+            description = "Path to the SSH identity file for this specific mount.";
+            example = "/home/user/.ssh/id_ed25519";
           };
           what = lib.mkOption {
             type = lib.types.str;
-            description = "The SFTP source.";
+            description = "The remote location to mount using SFTP (e.g., user@host:/path).";
+            example = "user@192.168.1.100:/";
           };
           where = lib.mkOption {
             type = lib.types.str;
-            description = "Mount point for the SFTP source.";
+            description = "Local directory where the remote location should be mounted.";
+            example = "/mnt/sftp";
           };
         };
       });
       default = [ ];
-      description = "List of directories to bind mount.";
+      description = "A list of SFTP mounts that define what remote locations to mount and where.";
       example = [
         {
           identityFile = "/home/user/.ssh/id_ed25519";
-          what = "user@192.168.1.100:/path/to/remote/dir";
-          where = "/mnt/my_sftp_mount";
+          what = "user@192.168.1.100:/";
+          where = "/mnt/sftp";
+        }
+      ];
+    };
+
+    binds = lib.mkOption {
+      type = lib.types.listOf (lib.types.submodule {
+        options = {
+          what = lib.mkOption {
+            type = lib.types.str;
+            description = "Local directory to be bind-mounted within the SFTP-mounted directory.";
+            example = "/mnt/sftp/documents";
+          };
+          where = lib.mkOption {
+            type = lib.types.str;
+            description = "Target directory where the bind mount should be placed.";
+            example = "/home/user/Documents";
+          };
+        };
+      });
+      default = [ ];
+      description = "A list of bind mounts for local directories, typically used within the SFTP mounts.";
+      example = [
+        {
+          what = "/mnt/sftp/docs";
+          where = "/home/user/Documents";
         }
       ];
     };
@@ -50,24 +76,32 @@ in
     # Enable sshfs package for mounting SSH drives
     system.fsPackages = [ pkgs.sshfs ];
 
-    # “fusermount3: option allow_other only allowed if ‘user_allow_other’ is set in /etc/fuse.conf”
+    # Required to allow 'allow_other' option for fuse
     programs.fuse.userAllowOther = true;
 
-    # Mount drives
-    fileSystems = lib.listToAttrs (map
-      (mount:
-        lib.nameValuePair mount.where {
-          device = mount.what;
-          fsType = "sshfs";
-          options = [
-            "IdentityFile=${mount.identityFile}"
-            "ServerAliveInterval=15"
-            "_netdev"
-            "allow_other"
-            "reconnect"
-            "x-systemd.automount"
-          ];
+    # Configure SFTP mounts and optional bind mounts
+    fileSystems = lib.listToAttrs
+      (map
+        (mount:
+          lib.nameValuePair mount.where {
+            device = mount.what;
+            fsType = "sshfs";
+            options = [
+              "IdentityFile=${mount.identityFile}"
+              "ServerAliveInterval=15"
+              "_netdev"
+              "allow_other"
+              "reconnect"
+              "x-systemd.automount"
+            ];
+          })
+        cfg.mounts
+      ) // lib.listToAttrs (map
+      (bind:
+        lib.nameValuePair bind.where {
+          device = bind.what;
+          options = [ "bind" ];
         })
-      cfg.mounts);
+      cfg.binds);
   };
 }
