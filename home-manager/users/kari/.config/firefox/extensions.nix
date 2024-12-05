0011f8fe6e621061f27770adcdcb0ed408b9508b
tupakkatapa <jesse@ponkila.com>
2024-12-05 08:07:46 +0200

    kari, ff: rm unused extensions

--- home-manager/users/kari/.config/firefox/extensions.nix
+++ home-manager/users/kari/.config/firefox/extensions.nix
@@ -6,10 +6,6 @@
     shortId = "vimium-ff";
   }
   {
-    uuid = "ff2mpv@yossarian.net";
-    shortId = "ff2mpv";
-  }
-  {
     uuid = "uBlock0@raymondhill.net";
     shortId = "ublock-origin";
   }
@@ -24,10 +20,6 @@
   {
     uuid = "{74145f27-f039-47ce-a470-a662b129930a}";
     shortId = "clearurls";
-  }
-  {
-    uuid = "savepage-we@DW-dev";
-    shortId = "save-page-we";
   }
   {
     uuid = "jid1-MnnxcxisBPnSXQ@jetpack";
@@ -54,32 +46,8 @@
     shortId = "enhancer-for-youtube";
   }
   {
-    uuid = "{ec8030f7-c20a-464f-9b0e-13a3a9e97384}";
-    shortId = "new-tab-override";
-  }
-  {
-    uuid = "{f209234a-76f0-4735-9920-eb62507a54cd}";
-    shortId = "unpaywall";
-  }
-  {
-    uuid = "{db420ff1-427a-4cda-b5e7-7d395b9f16e1}";
-    shortId = "to-deepl";
-  }
-  {
-    uuid = "simple-tab-groups@drive4ik";
-    shortId = "simple-tab-groups";
-  }
-  {
     uuid = "savewebpas@jeffersonscher.com";
     shortId = "save-webp-as-png-or-jpeg";
-  }
-  {
-    uuid = "{e4a8a97b-f2ed-450b-b12d-ee082ba24781}";
-    shortId = "greasemonkey";
-  }
-  {
-    uuid = "wayback_machine@mozilla.org";
-    shortId = "wayback-machine_new";
   }
   {
     uuid = "webextension@metamask.io";
