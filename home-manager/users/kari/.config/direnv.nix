f4c95a3f153d64b5549e55988eb4171559f0d924
tupakkatapa <jesse@ponkila.com>
2025-01-15 10:33:25 +0200

    direnv: try to fix whitelist

--- home-manager/users/kari/.config/direnv.nix
+++ home-manager/users/kari/.config/direnv.nix
@@ -2,9 +2,7 @@
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
-    config = {
-      # Automatically allow direnv
-      prefix = [ "/home/kari/Workspace" "/home/kari/nix-config" ];
-    };
+    # Automatically allow direnv
+    config.whitelist.prefix = [ "/home/kari/Workspace" "/home/kari/nix-config" ];
   };
 }
