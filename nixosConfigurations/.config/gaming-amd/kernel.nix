43f84b140f9daf8bb3c96d497f44ce95311e50f0
tupakkatapa <jesse@ponkila.com>
2025-03-25 01:16:34 +0200

    gaming-amd: test zen and kernel params

--- nixosConfigurations/.config/gaming-amd/kernel.nix
+++ nixosConfigurations/.config/gaming-amd/kernel.nix
@@ -1,9 +1,29 @@
 { config
+, pkgs
 , lib
 , ...
 }: {
-  # Enable clock and voltage adjustment for AMD GPU
-  boot.kernelParams = [ "amdgpu.ppfeaturemask=0xffffffff" ];
+  # Use Zen kernel
+  boot.kernelPackages = pkgs.linuxPackages_zen;
+
+  # Kernel parameters to optimize performance
+  boot.kernelParams = [
+    "mitigations=off"
+    "l1tf=off"
+    "mds=off"
+    "no_stf_barrier"
+    "noibpb"
+    "noibrs"
+    "nopti"
+    "nospec_store_bypass_disable"
+    "nospectre_v1"
+    "nospectre_v2"
+    "tsx=on"
+    "tsx_async_abort=off"
+
+    # Enable clock and voltage adjustment for AMD GPU
+    "amdgpu.ppfeaturemask=0xffffffff"
+  ];
 
   hardware = {
     # Xbox controller
