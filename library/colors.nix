f335deabe5cdf5480d1be22ed7c7c963607dd81f
tupakkatapa <jesse@ponkila.com>
2025-03-30 18:42:42 +0300

    library, colors: refactor + add ansi

--- library/colors.nix
+++ library/colors.nix
@@ -1,39 +1,52 @@
 # https://github.com/tinted-theming/base16-schemes
 {
-  # gruvbox-dark-medium
   gruvbox-dark-medium = {
-    base00 = "282828"; # Background 1
+    base00 = "282828"; # Background
     base01 = "3c3836"; # Background 2
-
     base02 = "504945"; # Inactive
-    base03 = "665c54";
-    base04 = "bdae93";
+    base03 = "665c54"; # Comments
+    base04 = "bdae93"; # Interface
     base05 = "d5c4a1"; # Foreground
     base06 = "ebdbb2"; # Accent
-    base07 = "fbf1c7";
-
+    base07 = "fbf1c7"; # Light
     base08 = "fb4934"; # Red
     base09 = "fe8019"; # Orange
     base0A = "fabd2f"; # Yellow
     base0B = "b8bb26"; # Green
-    base0C = "8ec07c"; # Aqua/Cyan
+    base0C = "8ec07c"; # Cyan
     base0D = "83a598"; # Blue
     base0E = "d3869b"; # Purple
     base0F = "d65d0e"; # Brown
   };
 
-  # washed-out-dracula
-  washed-out-dracula = {
-    base00 = "282936"; # Background 1
-    base01 = "6f7aa3"; # Background 2
+  dracula = {
+    base00 = "282936"; # Background
+    base01 = "3a3c4e"; # Background 2
+    base02 = "4d4f68"; # Inactive
+    base03 = "626483"; # Comments
+    base04 = "62d6e8"; # Interface
+    base05 = "e9e9f4"; # Foreground
+    base06 = "f1f2f8"; # Accent
+    base07 = "f7f7fb"; # Light
+    base08 = "ea51b2"; # Red
+    base09 = "b45bcf"; # Purple
+    base0A = "00f769"; # Yellow
+    base0B = "ebff87"; # Green
+    base0C = "a1efe4"; # Cyan
+    base0D = "62d6e8"; # Blue
+    base0E = "b45bcf"; # Purple
+    base0F = "00f769"; # Yellow
+  };
 
+  washed-out-dracula = {
+    base00 = "282936"; # Background
+    base01 = "6f7aa3"; # Background 2
     base02 = "92b6d4"; # Inactive
-    base03 = "7ccca8";
-    base04 = "ebcb8b";
+    base03 = "7ccca8"; # Comments
+    base04 = "ebcb8b"; # Interface
     base05 = "e9e9f4"; # Foreground
     base06 = "eaa5d7"; # Accent
-    base07 = "f7f7fb";
-
+    base07 = "f7f7fb"; # Light
     base08 = "d88383"; # Red
     base09 = "c2a1e1"; # Purple
     base0A = "d6d68d"; # Yellow
@@ -44,26 +57,60 @@
     base0F = "d6d68d"; # Yellow
   };
 
-  # ethereum-org
-  ethereum = {
-    base00 = "1c1c1c"; # Background 1
-    base01 = "2e2e2e"; # Background 1
-
+  ethereum-org = {
+    base00 = "1c1c1c"; # Background
+    base01 = "2e2e2e"; # Background 2
     base02 = "4a4a4a"; # Inactive
-    base03 = "5c5c5c";
-    base04 = "d0d0d0";
-    base05 = "eaeaea"; # Foregound
+    base03 = "5c5c5c"; # Comments
+    base04 = "d0d0d0"; # Interface
+    base05 = "eaeaea"; # Foreground
     base06 = "ceb0f9"; # Accent
-    base07 = "f0f0f0"; # Off-white
-
+    base07 = "f0f0f0"; # Light
     base08 = "e57373"; # Red
     base09 = "f4a261"; # Orange
     base0A = "f4e04d"; # Yellow
     base0B = "57c7b8"; # Green
-    base0C = "a5fcf4"; # Aqua/Cyan
+    base0C = "a5fcf4"; # Cyan
     base0D = "81a9f7"; # Blue
     base0E = "9d4edd"; # Purple
     base0F = "c44536"; # Brown
   };
-}
 
+  ansi = {
+    base00 = "000000"; # Background
+    base01 = "1a1a1a"; # Background 2
+    base02 = "333333"; # Inactive
+    base03 = "555555"; # Comments
+    base04 = "aaaaaa"; # Interface
+    base05 = "cccccc"; # Foreground
+    base06 = "eeeeee"; # Accent
+    base07 = "ffffff"; # Light
+    base08 = "ff0000"; # Red
+    base09 = "ff8000"; # Orange
+    base0A = "ffff00"; # Yellow
+    base0B = "00ff00"; # Green
+    base0C = "00ffff"; # Cyan
+    base0D = "0000ff"; # Blue
+    base0E = "ff00ff"; # Purple
+    base0F = "800000"; # Brown
+  };
+
+  washed-out-ansi = {
+    base00 = "000000"; # Background
+    base01 = "2a2a2a"; # Background 2
+    base02 = "454545"; # Inactive
+    base03 = "666666"; # Comments
+    base04 = "b0b0b0"; # Interface
+    base05 = "d5d5d5"; # Foreground
+    base06 = "eeeeee"; # Accent
+    base07 = "ffffff"; # Light
+    base08 = "ff6666"; # Red
+    base09 = "ffa366"; # Orange
+    base0A = "ffff66"; # Yellow
+    base0B = "66ff66"; # Green
+    base0C = "66ffff"; # Cyan
+    base0D = "6666ff"; # Blue
+    base0E = "ff66ff"; # Purple
+    base0F = "cc8066"; # Brown
+  };
+}
