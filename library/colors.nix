88776f418182b0a39054e6cf7309ab0d52e4b5c6
tupakkatapa <jesse@ponkila.com>
2025-04-03 05:29:40 +0300

    lib, colors: fix washed-out-dracula

--- library/colors.nix
+++ library/colors.nix
@@ -40,40 +40,21 @@
 
   washed-out-dracula = {
     base00 = "282936"; # Background
-    base01 = "6f7aa3"; # Background 2
-    base02 = "92b6d4"; # Inactive
-    base03 = "7ccca8"; # Comments
-    base04 = "ebcb8b"; # Interface
+    base01 = "585a6c"; # Background 2
+    base02 = "9699ac"; # Inactive
+    base03 = "a9aaba"; # Comments
+    base04 = "a9dee1"; # Interface
     base05 = "e9e9f4"; # Foreground
-    base06 = "eaa5d7"; # Accent
+    base06 = "f1f2f8"; # Accent
     base07 = "f7f7fb"; # Light
-    base08 = "d88383"; # Red
-    base09 = "c2a1e1"; # Purple
-    base0A = "d6d68d"; # Yellow
-    base0B = "7ccca8"; # Green
-    base0C = "92b6d4"; # Cyan
-    base0D = "6f7aa3"; # Blue
-    base0E = "c2a1e1"; # Purple
-    base0F = "d6d68d"; # Yellow
-  };
-
-  ethereum-org = {
-    base00 = "1c1c1c"; # Background
-    base01 = "2e2e2e"; # Background 2
-    base02 = "4a4a4a"; # Inactive
-    base03 = "5c5c5c"; # Comments
-    base04 = "d0d0d0"; # Interface
-    base05 = "eaeaea"; # Foreground
-    base06 = "ceb0f9"; # Accent
-    base07 = "f0f0f0"; # Light
-    base08 = "e57373"; # Red
-    base09 = "f4a261"; # Orange
-    base0A = "f4e04d"; # Yellow
-    base0B = "57c7b8"; # Green
-    base0C = "a5fcf4"; # Cyan
-    base0D = "81a9f7"; # Blue
-    base0E = "9d4edd"; # Purple
-    base0F = "c44536"; # Brown
+    base08 = "e2a1c1"; # Red
+    base09 = "c3a6d9"; # Purple
+    base0A = "66e7ad"; # Yellow
+    base0B = "e2eabc"; # Green
+    base0C = "b4e4df"; # Cyan
+    base0D = "a9dee1"; # Blue
+    base0E = "c3a6d9"; # Purple
+    base0F = "66e7ad"; # Yellow
   };
 
   ansi = {
