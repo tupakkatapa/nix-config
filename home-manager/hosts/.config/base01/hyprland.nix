c00eee66460362e362a51781f1c54b08be52dc55
Jesse Karjalainen <jesse@ponkila.com>
2025-06-17 11:48:35 +0300

    base01, hypr: fix split width (2)

--- home-manager/hosts/.config/base01/hyprland.nix
+++ home-manager/hosts/.config/base01/hyprland.nix
@@ -126,6 +126,7 @@ in
         force_split = 2; # Always split to the right
         preserve_split = true;
         pseudotile = true;
+        split_width_multiplier = "1.3";
       };
 
       # Startup
