515de2fbe51a59820dd54e8d2e4d26456602ff75
Jesse Karjalainen <jesse@ponkila.com>
2025-06-30 20:13:12 +0300

    torgue: add podman

--- nixosConfigurations/torgue/default.nix
+++ nixosConfigurations/torgue/default.nix
@@ -36,6 +36,17 @@
     ];
   };
 
+  # Podman
+  virtualisation.podman = {
+    enable = true;
+    dockerCompat = true;
+    defaultNetwork.settings.dns_enabled = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    podman-compose
+  ];
+
   # Saiko's automatic gc
   sys2x.gc.useDiskAware = true;
 
