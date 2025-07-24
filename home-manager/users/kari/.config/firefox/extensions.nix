94840c28f00eebd1134c9d805e42a3f34c2bd04f
Jesse Karjalainen <jesse@ponkila.com>
2025-07-24 20:27:36 +0300

    kari, ff: sort extensions

--- home-manager/users/kari/.config/firefox/extensions.nix
+++ home-manager/users/kari/.config/firefox/extensions.nix
@@ -1,21 +1,20 @@
 # https://discourse.nixos.org/t/declare-firefox-extensions-and-settings/36265/17
 # https://nur.nix-community.org/repos/rycee/
 [
+  # Navigation & Productivity
   {
     uuid = "{d7742d87-e61d-4b78-b8a1-b469842139fa}";
     shortId = "vimium-ff";
   }
   {
+    uuid = "{446900e4-71c2-419f-a6a7-df9c091e268b}";
+    shortId = "bitwarden-password-manager";
+  }
+
+  # Privacy & Security
+  {
     uuid = "uBlock0@raymondhill.net";
     shortId = "ublock-origin";
-  }
-  {
-    uuid = "{34daeb50-c2d2-4f14-886a-7160b24d66a4}";
-    shortId = "youtube-shorts-block";
-  }
-  {
-    uuid = "{e6e36c9a-8323-446c-b720-a176017e38ff}";
-    shortId = "torrent-control";
   }
   {
     uuid = "{74145f27-f039-47ce-a470-a662b129930a}";
@@ -29,32 +28,48 @@
     uuid = "jid1-KKzOGWgsW3Ao4Q@jetpack";
     shortId = "i-dont-care-about-cookies";
   }
+
+  # YouTube
   {
-    uuid = "addon@darkreader.org";
-    shortId = "darkreader";
+    uuid = "{34daeb50-c2d2-4f14-886a-7160b24d66a4}";
+    shortId = "youtube-shorts-block";
   }
   {
     uuid = "sponsorBlocker@ajay.app";
     shortId = "sponsorblock";
   }
   {
-    uuid = "{446900e4-71c2-419f-a6a7-df9c091e268b}";
-    shortId = "bitwarden-password-manager";
-  }
-  {
     uuid = "enhancerforyoutube@maximerf.addons.mozilla.org";
     shortId = "enhancer-for-youtube";
+  }
+
+  # Media & Downloads
+  {
+    uuid = "{e6e36c9a-8323-446c-b720-a176017e38ff}";
+    shortId = "torrent-control";
   }
   {
     uuid = "savewebpas@jeffersonscher.com";
     shortId = "save-webp-as-png-or-jpeg";
+  }
+
+  # Appearance
+  {
+    uuid = "addon@darkreader.org";
+    shortId = "darkreader";
+  }
+
+  # Other
+  {
+    uuid = "{e4a8a97b-f2ed-450b-b12d-ee082ba24781}";
+    shortId = "greasemonkey";
   }
   {
     uuid = "webextension@metamask.io";
     shortId = "ether-metamask";
   }
   {
-    uuid = "{e4a8a97b-f2ed-450b-b12d-ee082ba24781}";
-    shortId = "greasemonkey";
+    uuid = "{f209234a-76f0-4735-9920-eb62507a54cd}";
+    shortId = "unpaywall";
   }
 ]
