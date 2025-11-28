d3bfee4ad9aea4b0c07386e6074c9493a3433718
Jesse Karjalainen <jesse@ponkila.com>
2025-11-28 12:15:19 +0200

    rice01, zathura: recolor

--- home-manager/hosts/.config/base01/rice01/zathura.nix
+++ home-manager/hosts/.config/base01/rice01/zathura.nix
@@ -8,7 +8,40 @@ let
 in
 {
   programs.zathura.options = {
+    # Recolor (theme the PDF content)
+    recolor = true;
+    recolor-lightcolor = "#${colors.base00}"; # Background
+    recolor-darkcolor = "#${colors.base05}"; # Foreground text
+    recolor-keephue = true;
+
+    # UI colors
+    default-bg = "#${colors.base00}";
+    default-fg = "#${colors.base05}";
+
     statusbar-fg = "#${colors.base05}";
     statusbar-bg = "#${colors.base00}";
+
+    inputbar-bg = "#${colors.base00}";
+    inputbar-fg = "#${colors.base05}";
+
+    notification-bg = "#${colors.base00}";
+    notification-fg = "#${colors.base05}";
+    notification-error-bg = "#${colors.base08}";
+    notification-error-fg = "#${colors.base00}";
+    notification-warning-bg = "#${colors.base09}";
+    notification-warning-fg = "#${colors.base00}";
+
+    highlight-color = "#${colors.base0A}";
+    highlight-active-color = "#${colors.base0D}";
+
+    completion-bg = "#${colors.base00}";
+    completion-fg = "#${colors.base05}";
+    completion-highlight-bg = "#${colors.base02}";
+    completion-highlight-fg = "#${colors.base05}";
+
+    index-bg = "#${colors.base00}";
+    index-fg = "#${colors.base05}";
+    index-active-bg = "#${colors.base02}";
+    index-active-fg = "#${colors.base05}";
   };
 }
