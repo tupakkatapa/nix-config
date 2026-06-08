eadc942a59490653780337c93a9e1a019ee859cc
Jesse Karjalainen <jesse@ponkila.com>
2026-06-08 19:52:45 +0300

    vladof: rm cursor theme

--- home-manager/hosts/vladof/default.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, ... }:
-{
-  # Cursor theme for cage kiosk
-  home.pointerCursor = {
-    name = "Capitaine Cursors (Gruvbox)";
-    package = pkgs.capitaine-cursors-themed;
-    size = 32;
-  };
-}
